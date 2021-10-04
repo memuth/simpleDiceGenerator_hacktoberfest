f2880f0399ada43173541b48f2e8ddd61fe29073
memuth <60926408+memuth@users.noreply.github.com>
2021-10-04 17:54:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import random
+# Dice Generator using dictionary comprehensions
+from random import randint
+
 print("Welcome to the start of my code")
 print("I am a random dice generator, in case you don't have any or a few at home!")
-number_of_dices = int(input("How many dices do you need? (Type in numbers only!)"))
-for i in range(number_of_dices):
-  n = random.randint(1, 6)
-  print("Dice " + str(i+1) + " results: " + str(n))
+number_of_dices = int(input("How many dices do you need? (Type in numbers only!):"))
+result = {i+1: randint(1, 6) for i in range(number_of_dices)}
+print('\n'.join("Dice {}: Result {}".format(k, v) for k, v in result.items()))
 print("Here you go, thank you for using my dice program to generate what you need :)")
 # My Own Self-generated program for people who need dices at home :)
